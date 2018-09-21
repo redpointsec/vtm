000d496f1acaa96c3467aecd23b4524e5f2a8c76
Seth Law <seth.w.law@gmail.com>
2018-09-21 16:19:22 -0600

    disable csrf protections

--- taskManager/settings.py
+++ taskManager/settings.py
@@ -76,7 +76,7 @@ INSTALLED_APPS = (
 MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 )
