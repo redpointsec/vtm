0aa0e49388f058649acb5d95120570995b42bfef
Seth Law <seth.w.law@gmail.com>
2021-11-08 17:26:40 -0700

    project create test

--- taskManager/tests.py
+++ taskManager/tests.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from taskManager.models import Project
+from django.db import models
 from django.contrib.auth import authenticate
+from django.utils import timezone
+import datetime
 
 
 class UserTestCase(TestCase):
@@ -9,4 +13,15 @@ class UserTestCase(TestCase):
 
 	def test_user_create(self):
 		self.assertEqual(User.objects.filter(username="testuser").exists(),True)
-		
+
+class ProjectTestCase(TestCase):
+    def setUp(self):
+        Project.objects.create(
+            title="Test Project",
+            text="Test Description",
+            start_date = timezone.now(),
+            due_date = timezone.now() + datetime.timedelta(weeks=1)
+        )
+
+    def test_project_create(self):
+        self.assertEqual(Project.objects.filter(title="Test Project").exists(),True)
