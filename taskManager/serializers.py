0981fab4acfa5d2a64d97e39704d1535f379cc40
Seth Law <seth.w.law@gmail.com>
2024-10-31 17:49:55 -0600

    uuids now working in the API

--- taskManager/serializers.py
+++ taskManager/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework import routers, serializers, viewsets
+from rest_framework import serializers, viewsets
 from django.contrib.auth.models import User
 from taskManager.models import Task, Project, Notes, File, UserProfile
 from drf_spectacular.utils import extend_schema_view, extend_schema
@@ -22,13 +22,40 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
         return User.objects.filter(username=user.username)
 
 class TaskSerializer(serializers.HyperlinkedModelSerializer):
+    options = serializers.HyperlinkedRelatedField(
+    view_name='task-detail',
+    lookup_field = 'uuid',
+    many=True,
+    read_only=True)
+
     class Meta:
         model = Task
-        fields = ('uuid','url', 'title', 'text', 'start_date', 'due_date', 'completed', 'project', 'users_assigned')
+        fields = ('url', 'uuid', 'title', 'text', 'start_date', 'due_date', 'completed', 'project', 'users_assigned', 'options')
+        lookup_field = 'uuid'
+        extra_kwargs = {
+            'url': {'lookup_field': 'uuid'},
+            'project': {'lookup_field': 'uuid'}
+        }
+
+class TaskFilter(filters.FilterSet):
+    title = filters.CharFilter(lookup_expr='icontains')
+    text = filters.CharFilter(lookup_expr='icontains')
+    start_date = filters.DateFilter()
+    due_date = filters.DateFilter()
+    completed = filters.BooleanFilter()
+    project = filters.CharFilter(lookup_expr='icontains')
+    users_assigned = filters.CharFilter(lookup_expr='icontains')
+    
+    class Meta:
+        model = Task
+        fields = ['title', 'text', 'start_date', 'due_date', 'completed', 'project', 'users_assigned']
 
 class TaskViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    lookup_field = 'uuid'
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = TaskFilter
 
     def get_queryset(self):
         user = self.request.user
@@ -39,10 +66,29 @@ class TaskViewSet(viewsets.ReadOnlyModelViewSet):
 class ProjectSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Project
-        fields = ('uuid','url', 'title', 'text', 'start_date', 'due_date', 'priority', 'users_assigned')
+        fields = ('uuid', 'url', 'title', 'text', 'start_date', 'due_date', 'priority', 'users_assigned')
+        lookup_field = 'uuid'
+        extra_kwargs = {
+            'url': {'lookup_field': 'uuid'}
+        }
+
+class ProjectFilter(filters.FilterSet):
+    title = filters.CharFilter(lookup_expr='icontains')
+    text = filters.CharFilter(lookup_expr='icontains')
+    start_date = filters.DateFilter()
+    due_date = filters.DateFilter()
+    priority = filters.NumberFilter()
+    users_assigned = filters.CharFilter(lookup_expr='icontains')
+    
+    class Meta:
+        model = Project
+        fields = ['title', 'text', 'start_date', 'due_date', 'priority', 'users_assigned']
 
 class ProjectViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = ProjectSerializer
+    lookup_field = 'uuid'
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ProjectFilter
 
     def get_queryset(self):
         user = self.request.user
@@ -56,16 +102,20 @@ class NotesSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Notes
         fields = ('uuid', 'title', 'text', 'image', 'user', 'task')
+        lookup_field = 'uuid'
+        extra_kwargs = {
+            'url': {'lookup_field': 'uuid'},
+            'task': {'lookup_field': 'uuid'}
+        }
 
 class NotesFilter(filters.FilterSet):
     text = filters.CharFilter(lookup_expr='icontains')
     title = filters.CharFilter(lookup_expr='icontains')
     user = filters.CharFilter(lookup_expr='icontains')
-    task = filters.CharFilter(lookup_expr='icontains')
     
     class Meta:
         model = Notes
-        fields = ['title', 'text', 'user', 'task']
+        fields = ['title', 'text', 'user']
 
 class NotesViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Notes.objects.all()
@@ -74,30 +124,60 @@ class NotesViewSet(viewsets.ReadOnlyModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_class = NotesFilter
 
-    #def get_queryset(self):
-        # Filter by query parameters
-    #    uuid = self.request.query_params.get('uuid')
-    #    if uuid:
-    #        return Notes.objects.filter(uuid=uuid)
-    #    return Notes.objects.all()
-
 class FileSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = File
-        fields = ('uuid','url', 'name', 'path', 'project')
+        fields = ('uuid', 'name', 'path', 'project')
+        lookup_field = 'uuid'
+        extra_kwargs = {
+            'url': {'lookup_field': 'uuid'},
+            'project': {'lookup_field': 'uuid'}
+        }
+
+class FileFilter(filters.FilterSet):
+    name = filters.CharFilter(lookup_expr='icontains')
+    path = filters.CharFilter(lookup_expr='icontains')
+    project = filters.CharFilter(lookup_expr='icontains')
+    
+    class Meta:
+        model = File
+        fields = ['name', 'path', 'project']
 
 class FileViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = File.objects.all()
     serializer_class = FileSerializer
+    lookup_field = 'uuid'
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = FileFilter
 
 class UserProfileSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = UserProfile
-        fields = ('uuid','url', 'image', 'reset_token', 'reset_token_expiration', 'dob', 'ssn', 'user')
+        fields = ('uuid', 'image', 'reset_token', 'reset_token_expiration', 'dob', 'ssn', 'user')
+        lookup_field = 'uuid'
+        extra_kwargs = {
+            'url': {'lookup_field': 'uuid'}
+        }
+
+class UserProfileFilter(filters.FilterSet):
+    dob = filters.DateFilter()
+    ssn = filters.CharFilter(lookup_expr='icontains')
+    user = filters.CharFilter(lookup_expr='icontains')
+    
+    class Meta:
+        model = UserProfile
+        fields = ['dob', 'ssn', 'user']
+        lookup_field = 'uuid'
+        extra_kwargs = {
+            'url': {'lookup_field': 'uuid'}
+        }
 
 class UserProfileViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
+    lookup_field = 'uuid'
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = UserProfileFilter
 
     def get_queryset(self):
         user = self.request.user
