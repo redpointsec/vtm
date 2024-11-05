4b4c4399f217922b5e3d4d06c3235064c526046f
justinlarson <slimlarson@gmail.com>
2024-11-05 11:18:33 -0700

    ugh now its fixed

--- taskManager/settings.py
+++ taskManager/settings.py
@@ -175,7 +175,6 @@ EMAIL_PORT = 1025
 # REST framework settings
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
