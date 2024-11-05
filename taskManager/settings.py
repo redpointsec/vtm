a59e07c059e500c0b676c036c726d826cb3e7bf6
justinlarson <slimlarson@gmail.com>
2024-11-05 11:11:28 -0700

    fixed broken api tokens

--- taskManager/settings.py
+++ taskManager/settings.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = (
     'health_check.contrib.migrations',
     'health_check.contrib.psutil',
     'rest_framework',
+    'rest_framework.authtoken',
     'rest_framework_simplejwt',  # Added for JWT support
     'drf_spectacular',
 )
@@ -175,6 +176,7 @@ EMAIL_PORT = 1025
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
