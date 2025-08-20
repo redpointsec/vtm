c7f83a290634df0a6787c4287a14fc4d198b2c85
Justin Larson <slimlarson@gmail.com>
2025-08-20 15:17:35 -0600

    change

--- taskManager/views.py
+++ taskManager/views.py
@@ -14,7 +14,7 @@ import logging
 from django.http import (
     HttpResponse,  HttpResponseRedirect,
 )
-#make a change
+#make another change
 
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
